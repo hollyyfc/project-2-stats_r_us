1f71339aab86a0499eae743b0a82bc289edc3164
hollyyfc <yifancui@ucsb.edu>
2021-11-29 22:42:58 -0500

    Basic structure

--- App/colorblind-test.R
+++ App/colorblind-test.R
@@ -11,6 +11,7 @@ library(ggplotify)
 library(patchwork)
 library(ggtern)
 
+
 #constants ----
 #matrix values for c, RGB to LMS. c = change
 c.c = 0.31399022
@@ -79,21 +80,104 @@ proHex <- function(hex) {
   return(newHex)
 }
 
+deutHex <- function(hex) {
+
+  rgb.array = col2rgb(hex)
+
+  r1 = rgb.array[1] / 255
+  g1 = rgb.array[2] / 255
+  b1 = rgb.array[3] / 255
+
+  l = r1*c.c      + g1*c.cc   + b1*c.ccc
+  m = r1*cc.c     + g1*cc.cc  + b1*cc.ccc
+  s = r1*ccc.c    + g1*ccc.cc + b1*ccc.ccc
+
+  l1 = l
+  m1 = l*dd.d                 + s*dd.ddd
+  s1 =                          s
+
+  rNew = l1*u.u   + m1*u.uu   + s1*u.uuu
+  gNew = l1*uu.u  + m1*uu.uu  + s1*uu.uuu
+  bNew = l1*uuu.u + m1*uuu.uu + s1*uuu.uuu
+
+  rNew = round(case_when(rNew > 1 ~ 1, rNew < 0 ~ 0, TRUE ~ rNew) * 255)
+  gNew = round(case_when(gNew > 1 ~ 1, gNew < 0 ~ 0, TRUE ~ gNew) * 255)
+  bNew = round(case_when(bNew > 1 ~ 1, bNew < 0 ~ 0, TRUE ~ bNew) * 255)
+
+  newHex = toupper(rgb2hex(rNew, gNew, bNew))
+  return(newHex)
+}
+
+triHex <- function(hex) {
+
+  rgb.array = col2rgb(hex)
+
+  r1 = rgb.array[1] / 255
+  g1 = rgb.array[2] / 255
+  b1 = rgb.array[3] / 255
+
+  l = r1*c.c      + g1*c.cc   + b1*c.ccc
+  m = r1*cc.c     + g1*cc.cc  + b1*cc.ccc
+  s = r1*ccc.c    + g1*ccc.cc + b1*ccc.ccc
+
+  l1 = l
+  m1 =            + m
+  s1 = l*ttt.t    + m*ttt.tt
+
+  rNew = l1*u.u   + m1*u.uu   + s1*u.uuu
+  gNew = l1*uu.u  + m1*uu.uu  + s1*uu.uuu
+  bNew = l1*uuu.u + m1*uuu.uu + s1*uuu.uuu
+
+  rNew = round(case_when(rNew > 1 ~ 1, rNew < 0 ~ 0, TRUE ~ rNew) * 255)
+  gNew = round(case_when(gNew > 1 ~ 1, gNew < 0 ~ 0, TRUE ~ gNew) * 255)
+  bNew = round(case_when(bNew > 1 ~ 1, bNew < 0 ~ 0, TRUE ~ bNew) * 255)
+
+  newHex = toupper(rgb2hex(rNew, gNew, bNew))
+  return(newHex)
+}
+
+monoHex <- function(hex) {
+
+  rgb.array = col2rgb(hex)
+
+  r1 = rgb.array[1] / 255
+  g1 = rgb.array[2] / 255
+  b1 = rgb.array[3] / 255
+
+  l = r1*c.c      + g1*c.cc   + b1*c.ccc
+  m = r1*cc.c     + g1*cc.cc  + b1*cc.ccc
+  s = r1*ccc.c    + g1*ccc.cc + b1*ccc.ccc
+
+  l1 =                          s
+  m1 =                          s
+  s1 =                          s
+
+  rNew = l1*u.u   + m1*u.uu   + s1*u.uuu
+  gNew = l1*uu.u  + m1*uu.uu  + s1*uu.uuu
+  bNew = l1*uuu.u + m1*uuu.uu + s1*uuu.uuu
+
+  rNew = round(case_when(rNew > 1 ~ 1, rNew < 0 ~ 0, TRUE ~ rNew) * 255)
+  gNew = round(case_when(gNew > 1 ~ 1, gNew < 0 ~ 0, TRUE ~ gNew) * 255)
+  bNew = round(case_when(bNew > 1 ~ 1, bNew < 0 ~ 0, TRUE ~ bNew) * 255)
+
+  newHex = toupper(rgb2hex(rNew, gNew, bNew))
+  return(newHex)
+}
+
 #ui and server ----
 
 # ui
-## Add sliding size scale for plot?
 ui <- fluidPage(
   fluidRow(
-
-    column(2, colourInput("col1", "Choose 1st color"), submitButton("Update", icon("refresh"))),
+    column(2,
+           colourInput("col1", "Choose 1st color"),
+           submitButton("Update", icon("refresh"))
+           ),
     column(2, colourInput("col2", "Choose 2nd color")),
     column(2, colourInput("col3", "Choose 3rd color")),
     column(2, colourInput("col4", "Choose 4th color")),
     column(2, colourInput("col5", "Choose 5th color")),
-    column(2, colourInput("col6", "Choose 6th color"),
-
-    )
+    column(2, colourInput("col6", "Choose 6th color"))
   ),
   fluidRow(
     column(6,
@@ -108,7 +192,7 @@ ui <- fluidPage(
            plotOutput(outputId = "plot5")),
     column(6,
            plotOutput(outputId = "plot6"))
-    ),
+  ),
   fluidRow(
     column(12,
            verbatimTextOutput("result"))
@@ -118,19 +202,6 @@ ui <- fluidPage(
 
 # server
 server <- function(input, output, session){
-
-  # Output plots
-  # output$plot1 <- renderPlot({
-  #   ggplot(data = penguins,
-  #          aes(x = flipper_length_mm,
-  #              y = body_mass_g)) +
-  #     geom_point(aes(color = species,
-  #                    shape = species),
-  #                size = 3,
-  #                alpha = 0.8) +
-  #     theme_minimal() +
-  #     scale_color_manual(values = c(input$col1,input$col2,input$col3))
-  #   })
 
   # Output bars
   colorsquares <- data.frame(
@@ -149,48 +220,51 @@ server <- function(input, output, session){
     theme(plot.title = element_text(size = 25, hjust = 0.5, vjust = 3))
 
   output$plot1 <- renderPlot({
-
-      squareplot +
-      scale_fill_manual(values = c("blue", "red", "yellow", "green", "purple", "black")) +
-      labs(title = "test")
+    squareplot +
+      scale_fill_manual(values = c(input$col1, input$col2, input$col3,
+                                   input$col4, input$col5, input$col6)) +
+      labs(title = "Original")
   })
 
   output$plot2 <- renderPlot({
-
-      squareplot +
-      scale_fill_manual(values = c("blue", "red", "yellow", "green", "purple", "black")) +
-      labs(title = "test")
+    squareplot +
+      scale_fill_manual(values = c(input$col1, input$col2, input$col3,
+                                   input$col4, input$col5, input$col6)) +
+      labs(title = "Original")
   })
 
   output$plot3 <- renderPlot({
-
-      squareplot +
-      scale_fill_manual(values = c("blue", "red", "yellow", "green", "purple", "black")) +
-      labs(title = "test")
+    squareplot +
+      scale_fill_manual(values = c(proHex(input$col1), proHex(input$col2),
+                                   proHex(input$col3), proHex(input$col4),
+                                   proHex(input$col5), proHex(input$col6))) +
+      labs(title = "Protanopia")
   })
 
   output$plot4 <- renderPlot({
-
-      squareplot +
-      scale_fill_manual(values = c("blue", "red", "yellow", "green", "purple", "black")) +
-      labs(title = "test")
+    squareplot +
+      scale_fill_manual(values = c(deutHex(input$col1), deutHex(input$col2),
+                                   deutHex(input$col3), deutHex(input$col4),
+                                   deutHex(input$col5), deutHex(input$col6))) +
+      labs(title = "Deuteranopia")
   })
 
 
   output$plot5 <- renderPlot({
-
-      squareplot +
-      scale_fill_manual(values = c("blue", "red", "yellow", "green", "purple", "black")) +
-      labs(title = "test")
+    squareplot +
+      scale_fill_manual(values = c(triHex(input$col1), triHex(input$col2),
+                                   triHex(input$col3), triHex(input$col4),
+                                   triHex(input$col5), triHex(input$col6))) +
+      labs(title = "Tritanopia")
   })
 
   output$plot6 <- renderPlot({
-
-      squareplot +
-      scale_fill_manual(values = c("blue", "red", "yellow", "green", "purple", "black")) +
-      labs(title = "test")
+    squareplot +
+      scale_fill_manual(values = c(monoHex(input$col1), monoHex(input$col2),
+                                   monoHex(input$col3), monoHex(input$col4),
+                                   monoHex(input$col5), monoHex(input$col6))) +
+      labs(title = "Monochromatism")
   })
-
 
 
 
@@ -199,6 +273,7 @@ server <- function(input, output, session){
     input$col1, " ", input$col2, " ", input$col3, "...", "are the original colors\n",
     proHex(input$col1), " ", proHex(input$col2), " ", proHex(input$col3), "...", "are the protanaisfdfaef colors", sep = "") })
 }
+ ## Put under each chart!!!
 
 
 
